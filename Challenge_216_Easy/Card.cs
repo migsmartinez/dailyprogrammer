b963634f7c04f62cbfaf4d86d0bd6fa75b929b81
migsmartinez <migs_martinez@outlook.com>
2015-05-29 21:35:04 +1000

    Moved Card and Player classes out of Program.cs

---
+++ Challenge_216_Easy/Card.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_216_Easy
+{
+    public class Card
+    {
+        public CardValue Value { get; set; }
+        public Suit Suit { get; set; }
+    }
+
+    public enum Suit
+    {
+        Clubs = 1,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    public enum CardValue
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
